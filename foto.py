c3f45e2af5ba83505476b262c422106767211676
rafa <bustosmorenorafael@gmail.com>
2024-04-20 13:26:02 +0200

    Eliminados archivos innecesarios

--- foto.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from utils import PoseEstimator, Algorithm, MonocularMapper
-
-photo = cv2.imread('images/mayor.png')
-estimator = PoseEstimator('models/pose_landmarker_lite.task')
-mapper = MonocularMapper(1)
-algoritmo = Algorithm(estimator, mapper)
-algoritmo.run(photo, debug=True)
-

774cdaf321f69e81cac3499b876bc222512dd539
rafa <bustosmorenorafael@gmail.com>
2024-04-20 13:26:21 +0200

    Update file

--- 6_plane_visualization.py
+++ 6_plane_visualization.py
@@ -3,6 +3,6 @@ from utils import MonocularMapper, PointCloud3D, Image
 
 img = Image.from_file('images/prueba_rafa.png')
 
-cloud = PointCloud3D.get_cloud_from_image(MonocularMapper(2), img.image, True)
+cloud = PointCloud3D.get_cloud_from_image(MonocularMapper(3), img.image, True)
 # cloud.get_segmented_cloud()
 # cloud.draw_cloud()

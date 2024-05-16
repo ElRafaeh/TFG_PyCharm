77f1ce40daf6b0d0a77660e971c8341de73fb903
rafa <bustosmorenorafael@gmail.com>
2024-05-16 17:37:34 +0200

    Started new detector

--- FallDetector.py
+++ FallDetector.py
@@ -6,7 +6,7 @@ import numpy as np
 import telegram_send
 import asyncio
 
-algoritmo = Algorithm()  
+algoritmo = Algorithm(2)  
   
 class FallDetector:
   def __init__(self, camera: cv2.VideoCapture | Flask, server, telegram_report=True):
@@ -14,10 +14,12 @@ class FallDetector:
     self.telegram_report = telegram_report
     self.server = server
     
-    self.count = 0
+    self.fall_count = 0
+    self.no_fall_count = 0
     self.running = True
     self.image = None
     self.fall = None
+    self.fall_aux = None
     
   def run_camera(self):
     while self.camera.isOpened():
@@ -27,14 +29,13 @@ class FallDetector:
         break
       
       self.image = frame
-      self.fall = algoritmo.run(self.image)
+      self.fall_aux = algoritmo.run(self.image)
       
-      print(self.fall)
+      # print(self.fall)
       
       if self.telegram_report:
-        self.report()
+        self.fall = self.report()
     
-    return self.fall
   
   def show_image(self):
     while self.running:
@@ -47,13 +48,26 @@ class FallDetector:
     print('Cerrando aplicación')
     
   def report(self):
-    if self.fall:
-      self.count += 1
-      if self.count == 5:
+    filter = 10
+    print(self.fall_count, self.no_fall_count)
+    
+    if self.fall_aux:
+      self.fall_count += 1
+      self.no_fall_count = 0
+      
+      if self.fall_count == filter:
         _, img_encoded = cv2.imencode('.jpg', self.image)
         asyncio.run(telegram_send.send(captions=['⚠ ALERTA! Caida detectada!'], images=[img_encoded.tobytes()]))
-      return
-    self.count = 0
+        return True
+      elif self.fall_count > filter:
+        return True
+      return False 
+    else:
+      self.no_fall_count += 1
+      self.fall_count = 0
+      if self.no_fall_count >= filter:
+        return False
+      return True
     
     
   def detection_server_image(self):
@@ -83,55 +97,59 @@ class FallDetector:
     self.running = False
 
 
-class ProcessingServer:
-  def __init__(self, app: Flask, algorithm):
-    self.app = app
-    self.algorithm = algorithm
-    self.image = None
-    self.fall = None
-    self.thread = threading.Thread(target=self.show_image)
-    self.count = 0
-    self.running = True
-    self.thread.start()
-    
-  def run(self, telegram_report = True):
-    r = request    
-    nparr = np.frombuffer(r.data, np.uint8)
-    
-    self.image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    self.fall = algoritmo.run(self.image)
-    
-    if telegram_report:
-      self.report()
-    
-    return Response(response='RECEIVED', status=200, mimetype="text/xml")
-  
-  def report(self):
-    if self.fall:
-      self.count += 1
-      if self.count == 10:
-        _, img_encoded = cv2.imencode('.jpg', self.image)
-        asyncio.run(telegram_send.send(captions=['⚠ ALERTA! Caida detectada!'], images=[img_encoded.tobytes()]))
-      return
-    self.count = 0
-      
-  
-  def start(self):
-    self.app.add_url_rule('/processThisImage', 'processThisImage', self.run, methods=['POST'])
-    self.app.run(debug=True, host='0.0.0.0', use_reloader=False)
-    self.running = False
-
-  def show_image(self):
-    while self.thread.is_alive() and self.running:
-      if self.fall != None:
-        reported_imge = self.image.copy()
-        cv2.putText(reported_imge, "FALL: "+str(self.fall), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, ((0, 0, 255) if self.fall else (0, 255, 0)))
-        cv2.imshow('Detection', reported_imge)
-        cv2.waitKey(1)
-            
-    print('Cerrando aplicación')
         
 if __name__ == '__main__':
-  camera = cv2.VideoCapture('datasets/imvia/Coffee_room_02/Videos/video (51).avi') 
+  camera = cv2.VideoCapture('datasets/imvia/Home_02/Videos/video (39).avi') 
   fall_detector = FallDetector(camera, False, True)
   fall_detector.start()
+
+
+
+
+# class ProcessingServer:
+#   def __init__(self, app: Flask, algorithm):
+#     self.app = app
+#     self.algorithm = algorithm
+#     self.image = None
+#     self.fall = None
+#     self.thread = threading.Thread(target=self.show_image)
+#     self.count = 0
+#     self.running = True
+#     self.thread.start()
+    
+#   def run(self, telegram_report = True):
+#     r = request    
+#     nparr = np.frombuffer(r.data, np.uint8)
+    
+#     self.image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#     self.fall = algoritmo.run(self.image)
+    
+#     if telegram_report:
+#       self.report()
+    
+#     return Response(response='RECEIVED', status=200, mimetype="text/xml")
+  
+#   def report(self):
+#     if self.fall:
+#       self.count += 1
+#       if self.count == 10:
+#         _, img_encoded = cv2.imencode('.jpg', self.image)
+#         asyncio.run(telegram_send.send(captions=['⚠ ALERTA! Caida detectada!'], images=[img_encoded.tobytes()]))
+#       return
+#     self.count = 0
+      
+  
+#   def start(self):
+#     self.app.add_url_rule('/processThisImage', 'processThisImage', self.run, methods=['POST'])
+#     self.app.run(debug=True, host='0.0.0.0', use_reloader=False)
+#     self.running = False
+
+#   def show_image(self):
+#     while self.thread.is_alive() and self.running:
+#       if self.fall != None:
+#         reported_imge = self.image.copy()
+#         cv2.putText(reported_imge, "FALL: "+str(self.fall), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, ((0, 0, 255) if self.fall else (0, 255, 0)))
+#         cv2.imshow('Detection', reported_imge)
+#         cv2.waitKey(1)
+            
+#     print('Cerrando aplicación')
--- server.py
+++ server.py
@@ -10,7 +10,8 @@ from utils import Algorithm, MonocularMapper
 class ProcessingServer:
   def __init__(self, app: Flask, algorithm):
     self.app = app
-    self.algorithm = algorithm
+    self.algorithm: Algorithm = algorithm
+    self.landmarks = None
     self.image = None
     self.fall = None
     self.thread = threading.Thread(target=self.show_image)
@@ -22,19 +23,21 @@ class ProcessingServer:
     r = request    
     nparr = np.frombuffer(r.data, np.uint8)
     
-    self.image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    self.fall = algoritmo.run(self.image)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    self.fall = self.algorithm.run(img)
+    self.image = cv2.cvtColor(self.algorithm.image.image, cv2.COLOR_RGB2BGR)
+    self.landmarks = self.algorithm.landmarks
     
     if telegram_report:
-      self.report()
+      self.report(img)
     
     return Response(response='RECEIVED', status=200, mimetype="text/xml")
   
-  def report(self):
+  def report(self, img):
     if self.fall:
       self.count += 1
       if self.count == 10:
-        _, img_encoded = cv2.imencode('.jpg', self.image)
+        _, img_encoded = cv2.imencode('.jpg', img)
         asyncio.run(telegram_send.send(captions=['⚠ ALERTA! Caida detectada!'], images=[img_encoded.tobytes()]))
       return
     self.count = 0
@@ -50,13 +53,19 @@ class ProcessingServer:
       if self.fall != None:
         reported_imge = self.image.copy()
         cv2.putText(reported_imge, "FALL: "+str(self.fall), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, ((0, 0, 255) if self.fall else (0, 255, 0)))
+        
+        for i, landmark in enumerate(self.landmarks):
+            cv2.circle(reported_imge, (int(landmark[0]), int(landmark[1])), 1, (255, 0, 0), 2)
+            # cv2.scatter(int(landmark[0]), int(landmark[1]), c='#0CFF00', s=6)
+            cv2.putText(reported_imge, str(i), (int(landmark[0])+1, int(landmark[1])+1), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0, 255, 0), 1)
+        
         cv2.imshow('Detection', reported_imge)
         cv2.waitKey(1)
             
     print('Cerrando aplicación')
         
 if __name__ == '__main__':
-    algoritmo = Algorithm(mapper=MonocularMapper(1))
+    algoritmo = Algorithm(3)
     app = Flask(__name__)
     
     server = ProcessingServer(app, algoritmo)

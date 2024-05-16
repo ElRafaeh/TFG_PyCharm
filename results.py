3675401b3088c3cff98a7df90937a6735d2958ef
rafa <bustosmorenorafael@gmail.com>
2024-05-16 17:38:36 +0200

    minor changes

--- results.py
+++ results.py
@@ -1,5 +1,5 @@
 
-directory = 'Coffee_room_02'
+directory = 'Coffee_room_01'
 
 def accuracy(tp, fp, tn, fn):
   return ((tp + tn)/(tp + tn + fp + fn)) * 100
@@ -14,18 +14,18 @@ def specificity(fp, tn):
   return (tn/(tn + fp)) * 100
   
 
-with open(f'results/datasets/imvia/{directory}/results.txt', 'r') as file:
+with open(f'results/datasets/imvia/{directory}/results_1.txt', 'r') as file:
   lines = file.readlines()
 
 acc = 0
 sens = 0
 spec = 0
 count = 0
-
+time = 0
 
 for line in lines:
   data = line.split(' & ', )
-  time = float(data[2])
+  time += float(data[2])
   count = count + 1 
   
   TP = int(data[3])
@@ -39,6 +39,6 @@ for line in lines:
   
 
 
-print(f'{directory} & {round(acc/count, 2)} & {round(sens/count, 2)} & {round(spec/count, 2)} & {round(time/count, 2)} \\\\')
+print(f'{directory} & {round(acc/count, 2)} & {round(sens/count, 2)} & {round(spec/count, 2)} \\\\')
 # print(total / count)
 
